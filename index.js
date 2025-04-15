6af22b4d39c8022ebe3d4c59825f910493cfea14
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-04-15 10:52:28 +0530

    modify index.js file code

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ const port = 3000;
 env.config();
 
 const db = new pg.Client({
-  // user: process.env.PG_USER,
-  // host: process.env.PG_HOST,
-  // database: process.env.PG_DATABASE,
-  // password: process.env.PG_PASSWORD,
-  // port: process.env.PG_PORT,
-  connectionString: process.env.DB_URL,
-  ssl: {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  // connectionString: process.env.DB_URL,
+  // ssl: {
     
-    rejectUnauthorized: false
+  //   rejectUnauthorized: false
 
-  }
+  // }
 
 });
 
@@ -44,7 +44,6 @@ app.use(
     cookie: {
       
       maxAge: 1000 * 60 * 60 * 24,
-      secure: true
 
     }
   })
